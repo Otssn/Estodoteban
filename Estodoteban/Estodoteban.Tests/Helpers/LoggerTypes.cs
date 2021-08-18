17426106fec47c7753d05d2f9c063ce805fee9fc
Otssn <88810729+Otssn@users.noreply.github.com>
2021-08-17 21:57:30 -0500

    Unit test preparation

---
+++ Estodoteban/Estodoteban.Tests/Helpers/LoggerTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estodoteban.Tests.Helpers
+{
+    public enum LoggerTypes
+    {
+        Null,
+        List
+    }
+}
